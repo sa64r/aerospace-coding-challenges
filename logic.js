97f7fb455d406a4419032078a1d197cf43bc6d22
Sagar Shah <sa64r.shah@gmail.com>
2021-12-24 22:19:48 +0000

    problems 1 to 3 added

---
+++ logic.js
@@ -0,0 +1,25 @@
+// PROBLEM 1
+function calculateLift(rho, v, s, cl) {
+    //write your code here
+
+
+
+}
+
+// PROBLEM 2
+function calculateDrag(rho, v, s, cd) {
+    //write your code here  
+
+
+
+}
+
+
+// PROBLEM 3
+function calculateStallSpeed(m, g, s, cl, rho) {
+    //write your code here
+
+
+
+}
+
